c37a078a72f83dbac7b04b7e4dc245d12fefa344
Gondi László <laszlo.zsolt.gondi@gmail.com>
2022-10-04 15:08:47 +0200

    Add tests to Main.java for classes

--- src/hu/petrik/sokszog/Main.java
+++ src/hu/petrik/sokszog/Main.java
@@ -2,6 +2,18 @@ package hu.petrik.sokszog;
 
 public class Main {
     public static void main(String[] args) {
+        Sokszog s1 = new Sokszog(2);
+        System.out.println(s1);
 
+        Teglalap t1 = new Teglalap(2,3);
+        System.out.println(t1.getCircumference());
+        System.out.println(t1.getArea());
+        System.out.println(t1);
+
+        Haromszog h1 = new Haromszog(2,3,4);
+        System.out.println(h1.isEditable());
+        System.out.println(h1.getCircumference());
+        System.out.println(h1.getArea());
+        System.out.println(h1);
     }
 }
