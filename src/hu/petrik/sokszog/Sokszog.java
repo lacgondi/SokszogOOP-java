b65ee46fb34dbca4b5767051bca817407ea2a17a
Gondi László <laszlo.zsolt.gondi@gmail.com>
2022-10-04 14:07:17 +0200

    Add sokszog class

---
+++ src/hu/petrik/sokszog/Sokszog.java
@@ -0,0 +1,17 @@
+package hu.petrik.sokszog;
+
+public class Sokszog {
+    private double a;
+
+    public Sokszog(double a) {
+        this.a = a;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+}
