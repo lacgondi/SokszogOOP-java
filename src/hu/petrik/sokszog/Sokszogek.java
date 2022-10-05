bfd03793854e30b0cff7ec7d772b10ed5d47f3d4
László Gondi <laszlo.zsolt.gondi@gmail.com>
2022-10-05 16:23:45 +0200

    Make constructor in Sokszogek class

--- src/hu/petrik/sokszog/Sokszogek.java
+++ src/hu/petrik/sokszog/Sokszogek.java
@@ -6,5 +6,26 @@ import java.util.List;
 public class Sokszogek {
     private List<Sokszog> sokszogList = new ArrayList<>();
 
+    public Sokszogek() {
+        int rand = (int)(Math.random()*4)+1;
+        for (int i = 0; i < 100; i++) {
+            switch (rand){
+                case 1:
+                    sokszogList.add(new Haromszog());
+                    break;
+                case 2:
+                    sokszogList.add(new Paralelogramma());
+                    break;
+                case 3:
+                    sokszogList.add(new Deltoid());
+                    break;
+                case 4:
+                    sokszogList.add(new Teglalap());
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
 
 }
