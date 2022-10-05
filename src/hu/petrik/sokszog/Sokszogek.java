1573fd27ab08b14ab39f36e9d1d7d8f326f2806b
László Gondi <laszlo.zsolt.gondi@gmail.com>
2022-10-05 16:29:46 +0200

    Make functions in Sokszogek class

--- src/hu/petrik/sokszog/Sokszogek.java
+++ src/hu/petrik/sokszog/Sokszogek.java
@@ -1,6 +1,7 @@
 package hu.petrik.sokszog;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Sokszogek {
@@ -28,4 +29,40 @@ public class Sokszogek {
         }
     }
 
+    @Override
+    public String toString() {
+        String temp="";
+        for (int i = 0; i < sokszogList.size(); i++) {
+            temp+=sokszogList.toString()+"\n";
+        }
+        return temp;
+    }
+
+    public double sumCirc(){
+        double sum = 0;
+        for (int i = 0; i < sokszogList.size(); i++) {
+            sum+=sokszogList.get(i).getCircumference();
+        }
+        return sum;
+    }
+
+    public double sumArea(){
+        double sum = 0;
+        for (int i = 0; i < sokszogList.size(); i++) {
+            sum+=sokszogList.get(i).getArea();
+        }
+        return sum;
+    }
+
+    public int maxArea(){
+        int maxIndex = 0;
+        double max = 0;
+        for (int i = 0; i < sokszogList.size(); i++) {
+            if (sokszogList.get(i).getArea()>max){
+                max = sokszogList.get(i).getArea();
+                maxIndex = i;
+            }
+        }
+        return maxIndex;
+    }
 }
